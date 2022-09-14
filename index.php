5f4c32d15f8d5c46536ab5d324ccfcb997e5a875
mhanson048 <ubuntu@ip-172-31-20-68.ec2.internal>
2022-09-14 17:54:23 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<script type="text/javascript">
+  function check(cb) {
+    if (cb.checked)
+      alert('true!');
+    else
+      alert('false!');
+  }
+</script>
+
+<?php
+  ini_set('error_reporting', E_ALL);
+  ini_set('display_errors', true);
+  include "point.php";
+  include "pr.php";
+?>
+
+<?php
+  echo "Helloo";
+?>
---
+++ pr.php
@@ -0,0 +1,12 @@
+<?php
+$chk = '<input type="checkbox" onclick="check(this);" />';
+?>
+
+<?php
+for ($i = 0; $i < 10; $i++): ?>
+
+<div>
+  <?php echo $chk ?>
+</div>
+
+<?php endfor; ?>
