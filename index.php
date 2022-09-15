665d2d165e6e5695d0c33a483f8a6c5f262b1f08
mhanson048 <ubuntu@ip-172-31-20-68.ec2.internal>
2022-09-15 12:11:32 +0000

    rename some files

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <script type="text/javascript">
-  function check(cb) {
-    if (cb.checked)
-      alert('true!');
-    else
-      alert('false!');
+  function check(cb, sample) {
+    if (cb.checked) {
+      new Audio(sample).play()
+    }
   }
 </script>
 
@@ -11,9 +10,5 @@
   ini_set('error_reporting', E_ALL);
   ini_set('display_errors', true);
   include "point.php";
-  include "pr.php";
-?>
-
-<?php
-  echo "Helloo";
+  include "sample-track.php";
 ?>
--- sample-track.php
+++ sample-track.php
@@ -2,11 +2,15 @@
 $chk = '<input type="checkbox" onclick="check(this);" />';
 ?>
 
+<div style="display:flex;">
+
 <?php
 for ($i = 0; $i < 10; $i++): ?>
 
-<div>
-  <?php echo $chk ?>
-</div>
+  <div>
+    <?php echo $chk ?>
+  </div>
 
 <?php endfor; ?>
+
+</div>
