7516a24c658d29bd2111385cc628ad9fbba0c87b
mhanson048 <mathew.hanson@tylertech.com>
2022-09-15 14:46:11 +0000

    Working CRUD operations

--- mysql-test.php
+++ mysql-test.php
@@ -10,14 +10,41 @@
     die("connection failed: " . $conn->connect_error);
   }
 
-  $sql = "INSERT INTO Records (field1, field2)
+  $selectRecords = "Select * From Records";
+
+  $records = $conn->query($selectRecords);
+  if ($records->num_rows > 0) {
+    while ($row = $records->fetch_assoc()) {
+      $recordRow = "id: " . $row["id"] . " - field1: ";
+      $recordRow .= $row["field1"] . " - field2: ";
+      $recordRow .= $row["field2"] . " ";
+
+      $recordRow .= '<form method="post" style="display:inline-block;">';
+      $recordRow .= '<input type="submit" name="Delete" value="' . $row["id"] . '" />';
+      $recordRow .= '</form>' . '<br>';
+      echo $recordRow;
+    }
+  }
+
+  $createRecords = "INSERT INTO Records (field1, field2)
     VALUES ('field1', 'field2')";
 
   if(isset($_POST['Create'])) {
-    if ($conn->query($sql) === TRUE) {
+    if ($conn->query($createRecords) === TRUE) {
         echo "MySQL Connection Established";
     } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+        echo "Error: " . $createRecords . "<br>" . $conn->error;
+    }
+  }
+
+  $deleteRecord = 'Delete From Records Where id = ';
+
+  #var_dump($_POST['Delete'])
+  if(isset($_POST['Delete'])) {
+    if ($conn->query($deleteRecord . $_POST['Delete']) === TRUE) {
+        echo "Record Deleted";
+    } else {
+        echo "Error: " . $deleteRecords . "<br>" . $conn->error;
     }
   }
 
