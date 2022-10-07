34550df2b7a77561f20e189482046cc7e45a1c24
mhanson048 <mathew.hanson@tylertech.com>
2022-10-07 15:20:59 +0000

    error testing

--- classTest.php
+++ classTest.php
@@ -2,14 +2,11 @@
 
 namespace Studies\NS;
 
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', true);
-
 class TestClass {
   public $index = 1;
 
   public function increment() {
-    $this->index++;
+    $this->index/0;
   }
 
   public function getIndex() {
